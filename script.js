ea7fcf0c9e9e7b8a77abd847157a7c0a7d444df2
Petr Zajkas <petrzajkas@gmail.com>
2023-05-02 08:23:28 +0100

    overflow fixed, navbar hidden, responsivness final touch, footer logo updated

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const burger = document.querySelector('nav svg');
 
 burger.addEventListener('click', () => {
-    if(burger.classList.contains('active')){
+    if (burger.classList.contains('active')) {
         gsap.to('.links', { x: '100%' });
         gsap.to('.line', { stroke: 'white' });
         gsap.set('body', { overflow: 'auto' });
         gsap.set('body', { overflowX: 'hidden' });
-    }else{
+    } else {
         gsap.to('.links', { x: '0%' });
         gsap.to('.line', { stroke: 'black' });
         gsap.fromTo('.links a', { opacity: 0, y: 0 }, 
